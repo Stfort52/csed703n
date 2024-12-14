007277dc8790b1e46591a360ed70bf2e340c0549
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-14 16:23:19 +0900

    feat: implement custom model checkpoint callback

--- csed703n/model/utils/__init__.py
+++ csed703n/model/utils/__init__.py
@@ -1,3 +1,4 @@
+from .callback import EvenlySpacedModelCheckpoint as EvenlySpacedModelCheckpoint
 from .pe_from_name import pe_from_name as pe_from_name
 
-__all__ = ["pe_from_name"]
+__all__ = ["pe_from_name", "EvenlySpacedModelCheckpoint"]
