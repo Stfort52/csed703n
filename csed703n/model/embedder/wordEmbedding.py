e9571265ac9e15e8cec09d026a5983a646e1a9b3
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-14 16:21:43 +0900

    feat: allow biases in `LanguageModeling` and add support for `LayerNorm` eps

--- csed703n/model/embedder/wordEmbedding.py
+++ csed703n/model/embedder/wordEmbedding.py
@@ -2,10 +2,10 @@ from torch import LongTensor, Tensor, nn
 
 
 class WordEmbedding(nn.Module):
-    def __init__(self, vocab_size, embed_size):
+    def __init__(self, vocab_size, embed_size, ln_eps=1e-12):
         super(WordEmbedding, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.layer_norm = nn.LayerNorm(embed_size)
+        self.layer_norm = nn.LayerNorm(embed_size, ln_eps)
 
     def forward(self, x: LongTensor) -> Tensor:
         return self.layer_norm(self.embed(x))
--- csed703n/model/unembedder/languageModeling.py
+++ csed703n/model/unembedder/languageModeling.py
@@ -1,14 +1,15 @@
 from torch import Tensor, nn
+from torch.functional import F
 
 
 class LanguageModeling(nn.Module):
-    def __init__(self, d_model: int, n_vocab: int):
+    def __init__(self, d_model: int, n_vocab: int, ln_eps: float = 1e-12):
         super(LanguageModeling, self).__init__()
 
         self.dense = nn.Sequential(
-            nn.Linear(d_model, d_model), nn.GELU(), nn.LayerNorm(d_model)
+            nn.Linear(d_model, d_model), nn.GELU(), nn.LayerNorm(d_model, ln_eps)
         )
-        self.unembedding = nn.Linear(d_model, n_vocab, bias=False)
+        self.unembedding = nn.Linear(d_model, n_vocab, bias=True)
 
     def forward(self, x: Tensor) -> Tensor:
         # x: [b, n, d]
