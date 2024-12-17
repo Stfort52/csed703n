ac0209dd4942f66e9c157c3ffcbeb0b3750ed52e
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-17 18:50:01 +0900

    feat: implement `NerDataCollator`

--- csed703n/data/utils/__init__.py
+++ csed703n/data/utils/__init__.py
@@ -1,4 +1,5 @@
 from .cell import Cell as Cell
 from .mlmDataCollator import MlmDataCollator as MlmDataCollator
+from .nerDataCollator import NerDataCollator as NerDataCollator
 
-__all__ = ["Cell", "MlmDataCollator"]
+__all__ = ["Cell", "MlmDataCollator", "NerDataCollator"]
