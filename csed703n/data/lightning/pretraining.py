d066312e2d9ccb49dada8055211bdcb1e4597836
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-17 18:50:46 +0900

    fix: resolve issues with match-case ignoreing booleans

--- csed703n/data/lightning/pretraining.py
+++ csed703n/data/lightning/pretraining.py
@@ -13,14 +13,16 @@ class GenecorpusDataModule(L.LightningDataModule):
         self,
         dataset_dir: str | Path,
         token_dict: dict[str, int],
+        ignore_index: int | None = None,
         batch_size=32,
         shuffle: int | bool = 42,
         num_workers=4,
     ):
         super().__init__()
-        self.batch_size = batch_size
         self.dataset_dir = Path(dataset_dir)
         self.token_dict = token_dict
+        self.ignore_index = ignore_index
+        self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
 
@@ -31,18 +33,19 @@ class GenecorpusDataModule(L.LightningDataModule):
         )
 
         match self.shuffle:
-            case int():
-                self.dataset = self.dataset.shuffle(seed=self.shuffle)
             case True:
                 self.dataset = self.dataset.shuffle(seed=None)
             case False:
                 pass
+            case int():
+                self.dataset = self.dataset.shuffle(seed=self.shuffle)
 
         self.collator = MlmDataCollator(
             token_dict=self.token_dict,
             pad_token_or_index="<pad>",
             mask_token_or_index="<mask>",
             mask_prob=0.15,
+            ignore_index=self.ignore_index,
         )
 
     def train_dataloader(self):
