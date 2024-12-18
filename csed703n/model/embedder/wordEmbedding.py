55bcb6bca4b24be2ebe625075539bd3077cda325
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-18 11:02:21 +0900

    feat: dropout and type annotations in word embeddings

--- csed703n/model/embedder/wordEmbedding.py
+++ csed703n/model/embedder/wordEmbedding.py
@@ -2,10 +2,17 @@ from torch import LongTensor, Tensor, nn
 
 
 class WordEmbedding(nn.Module):
-    def __init__(self, vocab_size, embed_size, ln_eps=1e-12):
+    def __init__(
+        self,
+        vocab_size: int,
+        embed_size: int,
+        dropout_p: float = 0.0,
+        ln_eps: float = 1e-12,
+    ):
         super(WordEmbedding, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.layer_norm = nn.LayerNorm(embed_size, ln_eps)
+        self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, x: LongTensor) -> Tensor:
-        return self.layer_norm(self.embed(x))
+        return self.layer_norm(self.dropout(self.embed(x)))
