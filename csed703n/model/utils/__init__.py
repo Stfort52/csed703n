716a9122090e44696dd33d66b479a90d2aedb9f3
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2024-12-18 17:26:04 +0900

    feat: implement metrics

--- csed703n/model/utils/__init__.py
+++ csed703n/model/utils/__init__.py
@@ -1,11 +1,13 @@
 from .activation import activation_from_name as activation_from_name
 from .callback import EvenlySpacedModelCheckpoint as EvenlySpacedModelCheckpoint
 from .initalization import reset_weights as reset_weights
+from .metrics import create_metrics as create_metrics
 from .pe_from_name import pe_from_name as pe_from_name
 
 __all__ = [
     "pe_from_name",
     "EvenlySpacedModelCheckpoint",
     "reset_weights",
+    "create_metrics",
     "activation_from_name",
 ]
