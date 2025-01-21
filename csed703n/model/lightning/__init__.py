a400e5d0c9c7018adb6ed2abb5709a6ae2e43523
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2025-01-21 16:49:24 +0900

    feat: implement sequence classifier lightning

--- csed703n/model/lightning/__init__.py
+++ csed703n/model/lightning/__init__.py
@@ -1,6 +1,13 @@
 from .pretraining import LightningPretraining as LightningPretraining
+from .sequenceClassification import (
+    LightningSequenceClassification as LightningSequenceClassification,
+)
 from .tokenClassification import (
     LightningTokenClassification as LightningTokenClassification,
 )
 
-__all__ = ["LightningPretraining", "LightningTokenClassification"]
+__all__ = [
+    "LightningPretraining",
+    "LightningTokenClassification",
+    "LightningSequenceClassification",
+]
